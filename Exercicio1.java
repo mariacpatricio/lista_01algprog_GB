57bdd154001c957d69efde5bd4a992d280e31c6d
mariacpatricio <mariac.designgrafico@gmail.com>
2026-09-21 20:01:39 -0300

    Add files via upload

---
+++ Exercicio1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Exercicio1 {
+
+
+ public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    
+    System.out.print("Digite a primeira nota");
+    double nota1 = scanner.nextDouble();
+
+    System.out.print("Digite a segunda nota");
+    double nota2 = scanner.nextDouble();
+
+    System.out.print("Digite a terceira nota");
+    double nota3 = scanner.nextDouble();
+
+    double media = (nota1 + nota2 + nota3) / 3.0;
+
+    System.out.printf("Média das notas: " + media);
+
+    scanner.close();
+ }
+}
